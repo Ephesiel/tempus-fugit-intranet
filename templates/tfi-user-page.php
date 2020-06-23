8bf6daf51b53cd90296ee4e8f6497b77fdcae1e4
Benoît Huftier <ephesiel@gmail.com>
2020-06-23 14:35:28 +0200

    Update version 1.1.3

--- templates/tfi-user-page.php
+++ templates/tfi-user-page.php
@@ -9,6 +9,7 @@ $redirect_url = get_home_url();
  * Header file for the TFI user page plugins
  * 
  * @since 1.0.0
+ * @since 1.1.3     Add the parallax of tempus fugit site css. It's because I don't know how to add css with wordpress in a custom template
  */
 ?><!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?>>
@@ -16,6 +17,7 @@ $redirect_url = get_home_url();
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="<?php echo esc_attr( TFI_URL ) . 'assets/css/user-page.css'; ?>" />
+        <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() . 'assets/css/parallax.css'; ?>" />
 	</head>
 	<body>
 		<header id="tfi-user-page-header">
@@ -38,6 +40,5 @@ $redirect_url = get_home_url();
         <footer id="tfi-user-page-footer">
             <small><?php printf( esc_html__( 'Page load by plugin %s' ), '<b>' . esc_html__( 'Tempus Fugit Intranet' ) . '</b>' ); ?></small>
         </footer>
-        <script href="<?php echo esc_attr( TFI_URL ) . 'assets/css/user-page.css'; ?>"></script>
     </body>
 </html>
